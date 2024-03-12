6d607ecd06dc4e68b601c906ecfb33173aa4c167
t-mees03 <toby.mees.03@gmail.com>
2024-03-12 10:40:04 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+<section class="container-fluid about-bg text-center">
+        <div class="container">
+            <h2>GAMES &amp; LEVEL DESIGNER</h2>
+            <p class="particle">... and particle wizard</p>
+
+            <p class="about-text">My  name is Josh Whitkin. I’m a professional with more than 7 years of industry experience in 
+                Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games 
+                is the combination of technology  and creativity, and how multiple disciplines come together 
+                to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 
+                3D environments. I always tell a story.</p>
+            <div class="blocker"></div>
+
+        </div> <!-- container -->
+    </section>
+
+    <section class="container-fluid articles-bg">
+        <div class="container">
+            <h3 class="text-center">LATEST ARTICLES</h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://170.187.231.66/~mesh14/wp-content/themes/starter_for_josh_site/images/article_image_1.jpg" alt="Illustration of a woman in an action pose holding a bow.">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="article-date">Friday, November 10, 2030</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS 
+                        scholars are  starting to explore and intervene in the arts.  This object of study  
+                        brings  new challenges and  opportunities that we want  to explore in this session.   
+                        We would  like to gather first of all simply  new kinds of  knowledge arising from  STS 
+                        study  of the arts.</p>
+                        <a class="read-more" href="#">CONTINUE READING</a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://170.187.231.66/~mesh14/wp-content/themes/starter_for_josh_site/images/article_image_2.jpg" alt="3D renders of a sci-fi character model.">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="article-date">Friday, November 10, 2030</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS 
+                        scholars are  starting to explore and intervene in the arts.  This object of study  
+                        brings  new challenges and  opportunities that we want  to explore in this session.   
+                        We would  like to gather first of all simply  new kinds of  knowledge arising from  STS 
+                        study  of the arts.</p>
+                        <a class="read-more" href="#">CONTINUE READING</a>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://170.187.231.66/~mesh14/wp-content/themes/starter_for_josh_site/images/article_image_3.jpg" alt="Top down perspective of a pixelated race track.">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="article-date">Friday, November 10, 2030</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS 
+                        scholars are  starting to explore and intervene in the arts.  This object of study  
+                        brings  new challenges and  opportunities that we want  to explore in this session.   
+                        We would  like to gather first of all simply  new kinds of  knowledge arising from  STS 
+                        study  of the arts.</p>
+                        <a class="read-more" href="#">CONTINUE READING</a>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
