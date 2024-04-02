8e1624a74ef9dd852e72c10a1f3601acacae993e
t-mees03 <toby.mees.03@gmail.com>
2024-04-02 18:26:19 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -18,18 +18,17 @@
 
     <section class="container-fluid articles-bg">
         <div class="container">
-            <h3 class="text-center">LATEST ARTICLES</h3>
+            <h3 class="text-center">"><?php the_field('titleforarticles'); ?></h3>
             <div class="row">
-                <div class="col-md-4">
-                    <img class="img-fluid" src="http://170.187.231.66/~mesh14/wp-content/themes/starter_for_josh_site/images/article_image_1.jpg" alt="Illustration of a woman in an action pose holding a bow.">
-                    <h4 class="article-title">A PORTRAIT</h4>
-                    <p class="article-date">Friday, November 10, 2030</p>
-                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS 
-                        scholars are  starting to explore and intervene in the arts.  This object of study  
-                        brings  new challenges and  opportunities that we want  to explore in this session.   
-                        We would  like to gather first of all simply  new kinds of  knowledge arising from  STS 
-                        study  of the arts.</p>
-                        <a class="read-more" href="#">CONTINUE READING</a>
+                    <div class="col-md-4">
+                    <?php
+                    $image = get_field('articleimage');
+                    ?>
+                    <img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+                    <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+                    <p class="article-date"><?php the_field('articledate'); ?></p>
+                    <p class="article-p"><?php the_field('articleexcerpt'); ?></p>
+                    <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
                 <div class="col-md-4">
                     <img class="img-fluid" src="http://170.187.231.66/~mesh14/wp-content/themes/starter_for_josh_site/images/article_image_2.jpg" alt="3D renders of a sci-fi character model.">
