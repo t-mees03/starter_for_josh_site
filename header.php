aef036339700ea939fa10bd185f8379274d1355b
t-mees03 <toby.mees.03@gmail.com>
2024-04-02 19:35:47 +0800

    Update header.php

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 </head>
 <body>
 
-<header class="container-fluid bg1">
+<header class="container-fluid" style="background-size: cover; background-image: url(<?php the_field('headerbackgroundimage') ?>);">
     <div class="container">
     
     <nav class="navbar navbar-expand-md navbar-dark">
@@ -66,7 +66,7 @@ wp_nav_menu( array(
       </nav>
 
   <div class="tag">
-    <h1>Games Design for the future</h1>
+    <h1><?php the_field('maintagline'); ?></h1>
   </div> <!-- tag --> 
 
     </div> <!-- container -->
